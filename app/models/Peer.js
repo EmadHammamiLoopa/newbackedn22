7a08ecf9e1efbdcff1a857f3a6da272b583d2ff0
EmadHammami1 <mehammam@uio.no>
2025-06-02 20:27:56 +0200

    fixed the issues

--- app/models/Peer.js
+++ app/models/Peer.js
@@ -5,8 +5,6 @@ const peerSchema = new mongoose.Schema({
   userId:      { type: String, required: true, unique: true },
   peerId:      { type: String, required: true },
   lastUpdated: { type: Date,   default: Date.now },
-  expiresAt:   { type: Date,   default: () => Date.now() + 5 * 60_000 } // 5‑min ttl
 });
 
-peerSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 }); // Mongo deletes after 5 min
 module.exports = mongoose.model('Peer', peerSchema);
